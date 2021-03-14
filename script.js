e2df19f3d2183869a04a92f513248cd829cb1fc3
danielwestfall <76882475+danielwestfall@users.noreply.github.com>
2021-03-13 21:58:13 -0600

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ var requestOptions = {
 
 	fetch("https://api.tfl.gov.uk/Line/" + line + "/Route/Sequence/inbound?app_id=MIT%20xPro%20Homework&app_key=03cd84bdd6b540a8a6559c46c4ddf806", requestOptions)
 		.then(response => response.json())
-		.then(data => console.log(data.lineStrings.toLocaleString('en', { timeZone: 'UTC' }).substring(3,str.length - 3).split(,)))
+		.then(data => console.log(data.lineStrings.toLocaleString('en', { timeZone: 'UTC' }).substring(3,str.length - 3).split(',')))
 		.catch(error => console.log('error', error));
 		
 
