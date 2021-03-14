a517578b543a83459b410cf03a2779bdbb074cbf
danielwestfall <76882475+danielwestfall@users.noreply.github.com>
2021-03-14 14:24:30 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
-function switchLine(line){
-var myHeaders = new Headers();
-myHeaders.append("Cookie", "__cfduid=ddd8e249dcd6cce5220655528e9813aa21615584664");
-
-var requestOptions = {
-	method: 'GET',
-	headers: myHeaders,
-	redirect: 'follow'
-};
 
 
-	fetch("https://api.tfl.gov.uk/Line/" + line + "/Route/Sequence/inbound?app_id=MIT%20xPro%20Homework&app_key=03cd84bdd6b540a8a6559c46c4ddf806", requestOptions)
-		.then(response => response.json())
-		.then(data => console.log(data.lineStrings))
-		.catch(error => console.log('error', error));
-		
-		console.log(data);
+async function switchLine(line){
+	var myHeaders = new Headers();
+	myHeaders.append("Cookie", "__cfduid=ddd8e249dcd6cce5220655528e9813aa21615584664");
+	
+	var requestOptions = {
+		method: 'GET',
+		headers: myHeaders,
+		redirect: 'follow'
+
+	let url = "https://api.tfl.gov.uk/Line/" + line + "/Route/Sequence/inbound?app_id=MIT%20xPro%20Homework&app_key=03cd84bdd6b540a8a6559c46c4ddf806"
+
+	let response = await fetch(url , requestOptions);
+	var busJson = await response.json();
+	return busJson.data;
+}
+	let coordsLine = JSON.parse(busJson.data.lineStrings)
+	console.log(busJson);
 	map.on('load', function () {
 		map.addSource('route', {
 		'type': 'geojson',
@@ -23,7 +25,7 @@ var requestOptions = {
 		'properties': {},
 		'geometry': {
 		'type': 'MultiLineString',
-		'coordinates': data.lineStrings[0]
+		'coordinates': coordsline
 		}
 		}
 		});
